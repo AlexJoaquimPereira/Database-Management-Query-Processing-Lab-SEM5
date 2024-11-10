1fe61bf53a06f6bbb1a2b228c2393b02ac48322e
AlexJoaquimPereira <alexpereira1781@gmail.com>
2024-11-10 22:46:00 +0530

    SQL document for question 1

---
+++ Experiment 10/table_trigger.sql
@@ -0,0 +1,144 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: table_trigger
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `table1`
+--
+
+DROP TABLE IF EXISTS `table1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `table1` (
+  `a1` int(11) NOT NULL,
+  PRIMARY KEY (`a1`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table1`
+--
+
+LOCK TABLES `table1` WRITE;
+/*!40000 ALTER TABLE `table1` DISABLE KEYS */;
+INSERT INTO `table1` VALUES (101),(102),(104);
+/*!40000 ALTER TABLE `table1` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER table1_trigger
+AFTER INSERT ON table1
+FOR EACH ROW
+BEGIN
+    INSERT INTO table2 (a2) VALUES (NEW.a1);
+    DELETE FROM table3 WHERE a3 = NEW.a1;
+    IF EXISTS (SELECT 1 FROM table4 WHERE a4 = NEW.a1) THEN
+        UPDATE table4 SET b4 = b4 + 1 WHERE a4 = NEW.a1;
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `table2`
+--
+
+DROP TABLE IF EXISTS `table2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `table2` (
+  `a2` int(11) NOT NULL,
+  PRIMARY KEY (`a2`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table2`
+--
+
+LOCK TABLES `table2` WRITE;
+/*!40000 ALTER TABLE `table2` DISABLE KEYS */;
+INSERT INTO `table2` VALUES (101),(102),(104);
+/*!40000 ALTER TABLE `table2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `table3`
+--
+
+DROP TABLE IF EXISTS `table3`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `table3` (
+  `a3` int(11) NOT NULL,
+  PRIMARY KEY (`a3`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table3`
+--
+
+LOCK TABLES `table3` WRITE;
+/*!40000 ALTER TABLE `table3` DISABLE KEYS */;
+INSERT INTO `table3` VALUES (103);
+/*!40000 ALTER TABLE `table3` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `table4`
+--
+
+DROP TABLE IF EXISTS `table4`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `table4` (
+  `a4` int(11) NOT NULL,
+  `b4` int(11) DEFAULT 0,
+  PRIMARY KEY (`a4`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table4`
+--
+
+LOCK TABLES `table4` WRITE;
+/*!40000 ALTER TABLE `table4` DISABLE KEYS */;
+INSERT INTO `table4` VALUES (101,6),(104,4);
+/*!40000 ALTER TABLE `table4` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-24 17:16:19
